b6e2555e16facab7659b375a7a7647e123eb3773
tarteo <d.e.sluijk@gmail.com>
2025-03-16 13:08:32 +0100

    Refactor runboat-build-image.sh

--- src/runboat/kubefiles/curq/runboat-build-image.sh
+++ src/runboat/kubefiles/curq/runboat-build-image.sh
@@ -1,11 +1,12 @@
+# Login into ghcr.io
+echo $DOCKER_PASSWORD | docker login ghcr.io --username $DOCKER_USER --password-stdin
+
+# Enable explicit execution
 set -ex
 unset DOCKER_HOST
 
 # Install tools
 apk add curl tar
-
-# Login into ghcr.io
-echo $DOCKER_PASSWORD | docker login ghcr.io --username $DOCKER_USER --password-stdin
 
 # Get stuff
 curl -sSL https://github.com/${RUNBOAT_GIT_REPO}/tarball/${RUNBOAT_GIT_REF} | tar zxf - --strip-components=1
@@ -13,6 +14,7 @@ curl -sSL https://github.com/${RUNBOAT_GIT_REPO}/tarball/${RUNBOAT_GIT_REF} | ta
 # Start docker daemon
 dockerd --host=unix:///var/run/docker.sock &
 
+# Wait for dockerd
 until docker info > /dev/null 2>&1; do
   echo "Waiting for Docker daemon to start..."
   sleep 1
