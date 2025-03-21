3ab1e2837f9c4fb55c3f5b18f81f65b1456e7d8f
tarteo <d.e.sluijk@gmail.com>
2025-03-21 08:58:26 +0100

    Add installation of requirements from requirements.txt (if present)

--- src/runboat/kubefiles/runboat-clone-and-install.sh
+++ src/runboat/kubefiles/runboat-clone-and-install.sh
@@ -26,6 +26,10 @@ else
     exit 1
 fi
 
+if [[ -f requirements.txt ]] ; then
+    pip install -r requirements.txt
+fi
+
 # Keep a copy of the venv that we can re-use for shorter startup time.
 cp -ar /opt/odoo-venv/ /mnt/data/odoo-venv
 
